91dcd85120043d0d3e970142c3b82c23eb4a099a
Vorfis <120501074+Vorfis@users.noreply.github.com>
2024-06-30 13:49:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,21 +6,18 @@ def find_phone_number(filename, phone_number):
     reader = csv.reader(csvfile)
     for row in reader:
       if phone_number in row:
-        # Удаляем номер телефона из строки и возвращаем оставшуюся часть
+        
         row.remove(phone_number)
         return ', '.join(row)
   return None
 
-# Запрос номера телефона от пользователя
+
 phone_number = input("Введите номер телефона: ")
 
-# Имя CSV-файла
-filename = "EYEOFGOD telegram 774k.csv"  # Замените "contacts.csv" на имя вашего файла
+filename = "EYEOFGOD telegram 774k.csv"  
 
-# Поиск номера телефона
 result = find_phone_number(filename, phone_number)
 
-# Вывод результата
 if result:
   print(f"Найдено соответствие: {result}")
 else:
